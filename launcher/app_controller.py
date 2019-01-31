cd5e9e592c65cbe5cc8ed06193a7c972ccf89299
Herklos <herklos.dev@protonmail.com>
2019-01-31 21:10:45 +0100

    Implement first templates

--- launcher/app_controller.py
+++ launcher/app_controller.py
@@ -1,11 +1,11 @@
 from flask import jsonify, render_template
 
-from launcher import server_instance
+from launcher import server_instance, VERSION
 
 
 @server_instance.route("/")
 def home():
-    return render_template('index.html')
+    return render_template('index.html', launcher_version=VERSION)
 
 
 @server_instance.route("/update")
