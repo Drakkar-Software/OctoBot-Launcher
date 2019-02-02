5cf8caa9526f2ef118a1dc56b6b1f2647cf2496c
Herklos <herklos.dev@protonmail.com>
2019-02-02 11:45:34 +0100

    Add refresh feature

--- tests/entrypoint_test.py
+++ tests/entrypoint_test.py
@@ -1,5 +1,21 @@
-from launcher.launcher_entry import main
+import glob
+import os
+import shutil
+
+from entrypoint import LAUNCHER_PATH, main
+from launcher.launcher_entry import launcher
 
 
 def test_main():
-    main(["-nw"])
+    launcher(["-nw"])
+
+
+def test_install_bot():
+    launcher(["-nw", "-u"])
+    assert glob.glob("OctoBot*")
+
+
+def test_install_launcher():
+    shutil.rmtree(LAUNCHER_PATH)
+    main()
+    assert os.path.exists(LAUNCHER_PATH)
